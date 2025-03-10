d6ee54ce546fbae1cd143aa15db704c357a53743
Daniel Marin <damapa2003@gmail.com>
2025-03-11 00:11:51 +0100

    arreglos roles token

--- app/src/main/java/com/example/appmovilapi/task/TaskListScreen.kt
+++ app/src/main/java/com/example/appmovilapi/task/TaskListScreen.kt
@@ -39,6 +39,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.appmovilapi.Task
+import com.example.appmovilapi.utils.decodeJwt
+import com.example.appmovilapi.utils.getRolesFromJwt
 import com.example.appmovilapi.utils.obtenerRolesDesdeToken
 import kotlinx.coroutines.launch
 
@@ -56,9 +58,25 @@ fun TaskListScreen(
 
     val errorShown = remember { mutableStateOf(false) }
 
-    val roles = obtenerRolesDesdeToken(token)
+    val publicKey = """
+        -----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvcjjvMc4R7zvSMCCol1G
+HcHUFKwV+sTn6aM8II6TT2vPIPyhVOG0oiykTHNCoE3rTH/XSOHz7Nek2vg5RgmP
+DOPrDQEm7UFRedEPy5NBCthA2j8/ZN0KJ3pGkguV4GRAFlXJzMFAx36HpDOpnWYP
+6zkurq6r/+AyZJ5HGrsU9vGe5Rgs9SOwvq+1cXuioSZmTp3klZpbQrwwi8cNBBU+
+lfW+reJvLXDffUSMVAX4iGRoJceXIWNcWj+xVXe7L182OP1E0oSHDCYfgzKF1zlA
+EhCSvsjK8MbbWrFUL9xig4WIUd6qx78vv16N+2k7kOsObNhvjZ4GzTHBXLplBLjI
+HwIDAQAB
+-----END PUBLIC KEY-----
+    """.trimIndent()
 
-    println(roles)
+    val decodedJWT = decodeJwt(token, publicKey)
+    var roles = listOf("")
+
+    decodedJWT?.let {
+        roles = getRolesFromJwt(it)
+        println("Roles: $roles")
+    } ?: println("Token invalido o firma incorrecta")
 
     LaunchedEffect(key1 = Unit) {
         if (username.isNotEmpty() && token.isNotEmpty()) {
--- app/src/main/java/com/example/appmovilapi/utils/tokenDecoder.kt
+++ app/src/main/java/com/example/appmovilapi/utils/tokenDecoder.kt
@@ -1,13 +1,53 @@
 package com.example.appmovilapi.utils
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import com.auth0.jwt.interfaces.RSAKeyProvider
+import java.security.KeyFactory
+import java.security.PublicKey
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.X509EncodedKeySpec
+import java.util.Base64
 
-fun obtenerRolesDesdeToken(token: String): List<String> {
+class SimpleRSAKeyProvider(private val publicKey: RSAPublicKey): RSAKeyProvider{
+    override fun getPublicKeyById(keyId: String?): RSAPublicKey = publicKey
+    override fun getPrivateKey(): RSAPrivateKey? = null
+    override fun getPrivateKeyId(): String? = null
+}
+
+fun decodeJwt(token: String, publicKeyPEM: String): DecodedJWT? {
     return try {
-        val jwt = JWT.decode(token)
-        val rolesClaim = jwt.getClaim("roles").asList(String::class.java) // Extrae el claim "roles"
-        rolesClaim ?: emptyList()
+        val publicKeyBytes = Base64.getDecoder().decode(publicKeyPEM.replace("-----BEGIN PUBLIC KEY-----","")
+            .replace("-----END PUBLIC KEY-----", "").replace("\n", ""))
+        val keySpec = X509EncodedKeySpec(publicKeyBytes)
+        val keyFactory = KeyFactory.getInstance("RSA")
+        val publicKey = keyFactory.generatePublic(keySpec) as RSAPublicKey
+
+        val algorithm = Algorithm.RSA256(SimpleRSAKeyProvider(publicKey))
+
+        JWT.require(algorithm).build().verify(token)
     } catch (e: Exception) {
-        emptyList() // Si hay un error, retorna una lista vacía
+        println("Error decoding JWT: ${e.message}")
+        null
+    }
+}
+
+fun getRolesFromJwt(decodedJWT: DecodedJWT, claimName: String = "roles"): List<String>{
+    return try {
+        val claim = decodedJWT.getClaim(claimName)
+        if(!claim.isMissing && !claim.isNull){
+            if (claim.asList(String::class.java) != null){
+                claim.asList(String::class.java)
+            } else {
+                listOf(claim.asString())
+            }
+        } else {
+            emptyList()
+        }
+    }catch (e:Exception){
+        println("Error extrayendo roles: ${e.message}")
+        emptyList()
     }
 }
