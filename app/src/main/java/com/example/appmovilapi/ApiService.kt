51f111c85e5fe4324e8b27ab0fa570194c68cddc
Daniel Marin <damapa2003@gmail.com>
2025-03-10 23:16:42 +0100

    delay peticion api

--- app/src/main/java/com/example/appmovilapi/ApiService.kt
+++ app/src/main/java/com/example/appmovilapi/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.appmovilapi
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
@@ -9,6 +10,7 @@ import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
+import java.util.concurrent.TimeUnit
 
 interface ApiService {
     @POST("/usuario/login")
@@ -39,9 +41,16 @@ interface ApiService {
     companion object {
         private const val BASE_URL = "https://api-rest-segura2-xres.onrender.com"
 
+        val client = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
         fun create(): ApiService {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(ApiService::class.java)
